46bc7ccbf0a32b19bff14de2a5ce062ad7e95ec7
Rowland <rowlandz@outlook.com>
2020-04-28 23:03:05 -0700

    Allow extension to inject code without user activation. Restrict effects to twitter.com

--- src/background.js
+++ src/background.js
@@ -1,9 +1,17 @@
 // this is the background code...
 
 // listen for our browerAction to be clicked
-chrome.browserAction.onClicked.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.id, {
-		file: 'src/inject.js'
-	});
-});
+// chrome.browserAction.onClicked.addListener(function (tab) {
+// 	// for the current tab, inject the "inject.js" file & execute it
+// 	chrome.tabs.executeScript(tab.id, {
+// 		file: 'src/inject.js'
+// 	});
+// });
+
+chrome.runtime.onMessage.addListener(function(message, callback) {
+    if (message.data == "inject") {
+		chrome.tabs.executeScript({
+			file: 'src/inject.js'
+		});
+	}
+  });
